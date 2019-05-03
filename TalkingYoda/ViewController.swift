a653eb7e6aa81cc35b8b9fceb4120cff0d60b42a
John Gallaugher <john.gallaugher@bc.edu>
2019-05-03 19:19:36 -0400

    Initial Commit

---
+++ TalkingYoda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TalkingYoda
+//
+//  Created by John Gallaugher on 5/3/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
